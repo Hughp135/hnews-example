05e9ac12f0add1804a5babbf5aa7523f23ad5327
Hugh <12624925+Hughp135@users.noreply.github.com>
2023-02-06 19:27:04 +0900

    Add scss, header layout

---
+++ app/components/Header/Header.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import styles from "./styles.module.scss";
+import headerBgImage from "../../../public/headerbg.jpg";
+
+export const Header = () => {
+  return (
+    <header className={styles.header}>
+      <Image
+        className={styles.bgImage}
+        alt="Hacker News Top 100"
+        src={headerBgImage}
+        fill
+      />
+      <h1>Hacker News Top 100 Stories</h1>
+    </header>
+  );
+};
